4acfe22d41bf56ec8f2908a67d2315c0e4ce78c6
FlorApreda <florencia.apreda@gmail.com>
2020-11-02 21:20:51 -0300

    delete employee

--- js/funcionalidades.js
+++ js/funcionalidades.js
@@ -21,7 +21,7 @@ const cargarUsuarios = async() => {
             tdEMail.innerText = user.email;
             tdAddress.innerText = user.address;
             tdPhone.innerText = user.phone;   
-            
+            tr.setAttribute("id", user.id)
             const checkBox = document.createElement("input");
             checkBox.type = "checkbox";
             const edit = document.createElement("button");
@@ -31,6 +31,8 @@ const cargarUsuarios = async() => {
             eliminar.type = "button";
             eliminar.innerText = "Delete";
             
+            eliminar.addEventListener('click', () => eliminarUsuario(user));
+
             tdActions.appendChild(edit);
             tdActions.appendChild(eliminar);
             tdCheckBox.appendChild(checkBox);
@@ -48,6 +50,44 @@ const cargarUsuarios = async() => {
         console.log("ERROR", err);
     }      
 };
+
+const eliminarUsuario = (user) => {
+    const eliminarConfirmado = async () => {
+        try{
+            await axios.delete(`https://5f7c70d600bd74001690ac5e.mockapi.io/users/${user.id}`)
+            const row = document.getElementById(user.id);
+            row.remove();
+            closeModal();
+        } catch(err){
+            console.error(err);
+        }
+    }
+
+    const closeModal = () => {
+        modal.parentNode.classList.add("closed");
+        titulo.remove();
+        btnConfirmar.remove();
+        btnCancelar.remove();
+    }
+
+    const modal = document.querySelector(".modal-delete");
+    const titulo = document.createElement("h2");
+    const btnConfirmar = document.createElement("button");
+    const btnCancelar = document.createElement("button");
+    modal.parentNode.classList.remove("closed");
+    
+    titulo.innerText = `¿Está segurx que desea eliminar a ${user.fullname} de la lista?`;
+    btnConfirmar.innerText = "Eliminar";
+    btnConfirmar.classList.add("btn-delete")
+    btnCancelar.innerText = "Cancelar";
+    btnCancelar.classList.add("btn-cancelar")
+
+    modal.appendChild(titulo);
+    modal.appendChild(btnConfirmar);
+    modal.appendChild(btnCancelar);
+    btnConfirmar.addEventListener('click', eliminarConfirmado)
+    btnCancelar.addEventListener('click', closeModal)
+}
 
 const load = () => {
     cargarUsuarios();

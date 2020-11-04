2201d500fdffbd7a30c0710de8a3c3495cefce26
JumiBerri <jb_jaureguiberri@hotmail.com>
2020-11-04 01:02:40 -0300

    addUser module

--- src/js/addUser.js
+++ src/js/addUser.js
@@ -1,7 +1,31 @@
 const addUser = () => {
     const buttonAdd = document.querySelector("#add-new-employee");
-    buttonAdd.addEventListener("click", () => {
-        openAddModal();
+    buttonAdd.addEventListener("click", async(e) => {
+        openAddModal();   
         closeModal();
+
+        const user = dataUsers.find(u => u.id === id);
+        document.querySelector("#name").value = user.fullname; 
+        document.querySelector("#email").value = user.email; 
+        document.querySelector("#address").value = user.address; 
+        document.querySelector("#phone").value = user.phone;   
+
+        e.preventDefault();
+        formValidation();    
+            try {
+                if(valueName !== undefined && valueEmail !== undefined && valueAddress !== undefined && valuePhone !== undefined) {
+                    const user = {
+                        fullname: valueName,
+                        email: valueEmail,
+                        address: valueAddress,
+                        phone: valuePhone
+                    }
+                    const res = await axios.post(BASE_URL + id, user)
+                    alert("User was added succesyarn sfully!")
+                    return res.data;
+                }
+            }catch(err) {
+                alert("ERROR: it can't add it", err);
+            }  
     });
 }
